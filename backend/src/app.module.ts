8be81aeb6004321ab4b4528c72c4e69d95c2677f
sokun1102 <ngophongnguyn2004@gmail.com>
2026-04-03 16:15:48 +0700

    feat: add microservices guide and user story

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,6 +25,8 @@ import { ProductsModule } from './products/products.module';
       }),
     }),
     ProductsModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'; // Import Guard
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  // API này AI CŨNG CÓ THỂ GỌI (Không có Guard)
   @Get()
   async findAll() {
     try {
@@ -23,6 +25,8 @@ export class ProductsController {
     return this.productsService.findOne(+id);
   }
 
+  // API NÀY ĐÃ BỊ KHOÁ: CHỈ AI CÓ TOKEN (ĐĂNG NHẬP RỒI) MỚI ĐƯỢC TẠO SẢN PHẨM MỚI
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createProductDto: any) {
     return this.productsService.create(createProductDto);
